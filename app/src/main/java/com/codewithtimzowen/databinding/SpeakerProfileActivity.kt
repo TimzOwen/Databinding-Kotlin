8d137410d8a819a0d1d76f898702d7d020ecd866
Timz Owen <ktimothy@kabarak.ac.ke>
2021-09-07 14:48:09 +0300

    updated menu icons

--- app/src/main/java/com/codewithtimzowen/databinding/SpeakerProfileActivity.kt
+++ app/src/main/java/com/codewithtimzowen/databinding/SpeakerProfileActivity.kt
@@ -1,7 +1,11 @@
 package com.codewithtimzowen.databinding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.codewithtimzowen.databinding.databinding.ActivitySpeakerProfileBinding
 
 class SpeakerProfileActivity : AppCompatActivity() {
@@ -26,8 +30,23 @@ class SpeakerProfileActivity : AppCompatActivity() {
         bindingSpeaker.tvTopic.setText(topic)
         bindingSpeaker.role.setText(role)
 
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.icon_share -> shareMeetUp()
+            R.id.icon_home -> startActivity(Intent(this@SpeakerProfileActivity, MainActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
+    // open a an activity to share intents
+    private fun shareMeetUp() {
+        Toast.makeText(this, "No links yet....", Toast.LENGTH_SHORT).show()
     }
 }
